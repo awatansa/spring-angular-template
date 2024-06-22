5b16fddc5a106c6e0f079932329c29bf1d3b597d
Awatansa Vishwakarma <ravi.awatansa@gmail.com>
2024-06-23 04:46:54 +0530

    Initialize the project
    - Added spring boot backend
    - added angular frontend

---
+++ src/main/java/com/vyira/learning/template/SpringAngularTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.vyira.learning.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAngularTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAngularTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vyira/learning/template/SpringAngularTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vyira.learning.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAngularTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
